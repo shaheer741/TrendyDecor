0e3908930f661c9889ba475bf54d9f7c4b5c5ba6
shaheer <shaheerkhalid10@gmail.com>
2023-05-04 00:19:59 +0500

    pc latest push
    white arrow issue resolved

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import SingleProduct from './pages/SingleProduct';
+import HomePage from './pages/Homepage'
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
